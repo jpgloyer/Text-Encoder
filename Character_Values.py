e050d23ab55be87198e61667366c9d268b236147
J. Pierce Gloyer <jpgloyer@gmail.com>
2021-12-11 00:23:53 -0600

    Mostly Done

--- Character_Values.py
+++ Character_Values.py
@@ -1,15 +1,35 @@
-Character_List1 = [chr(i) for i in range(65,91)]
-Character_List2 = [chr(i) for i in range(97,123)]
-Character_List3 = [chr(i) for i in range(48,58)]
-Int_Value_Of_Character = [i for i in range(62)]
-Full_Character_List = []
-for i in range(len(Character_List3)):
-    Full_Character_List.append(Character_List3[i])
-for i in range(len(Character_List2)):
-    Full_Character_List.append(Character_List2[i])
-for i in range(len(Character_List1)):
-    Full_Character_List.append(Character_List1[i])
+def get_chars():
+    #To be used once at the beginning of every run of the program
+    #Simply generates a list of standard characters (numbers, lower and capital case letters)
+    #Returns the completed list, doubled
+    Character_List1 = [chr(i) for i in range(65,91)]
+    Character_List2 = [chr(i) for i in range(97,123)]
+    Character_List3 = [chr(i) for i in range(48,58)]
+    Full_Character_List = []
+    for i in range(len(Character_List3)):
+        Full_Character_List.append(Character_List3[i])
+    for i in range(len(Character_List2)):
+        Full_Character_List.append(Character_List2[i])
+    for i in range(len(Character_List1)):
+        Full_Character_List.append(Character_List1[i])
+
+    for i in range(len(Full_Character_List)):
+        Full_Character_List.append(Full_Character_List[i])
 
 
-for i in range(len(Int_Value_Of_Character)):
-    print(Full_Character_List[i],end='')
+    # for i in range(len(Int_Value_Of_Character)):
+    #     print(Full_Character_List[i],end='')
+    return Full_Character_List
+
+
+def char_input_output_encrypting(input_char, encryption_value, Character_List):
+    index = 0
+    i = 0
+    while i != -1:
+        if input_char == Character_List[i]:
+            index = i
+            i = -1
+        else:
+            i+=1
+
+    return Character_List[index+encryption_value]
--- Main.py
+++ Main.py
@@ -1,36 +1,66 @@
-def character_list_generator():
+import Character_Values
+
+def message_list_generator():
     character_list = []
-    encrypted_character_list = []
-    increment_val = 0
     file = open('file.txt', 'r')
- 
     end_of_file = False
-    while not end_of_file:
-     
+
+    while not end_of_file: 
         # read by character
         character = file.read(1)         
         if not character:
             end_of_file = True
             
-        character_list.append(character)
-        print(character)
+
+        #print(character)
         if character:
-            new_char = chr(ord(character)+increment_val)
-            encrypted_character_list.append(chr(ord(character)+increment_val))
-            increment_val += 1
-            
-    
+            character_list.append(chr(ord(character)))
+
     file.close()
 
-    print(character_list)
-    print()
-    print(encrypted_character_list)
-    return encrypted_character_list
+    #print(character_list)
+    #print()
+    #print(encrypted_character_list)
+    return character_list
 
-def decryption(character_list, key):
-    print('nah')
+def encryption(Message_Character_List, key, Character_List):
+    if key < 0:
+        key = -key
+    if key > 61:
+        key = key % 61
+    Encrypted_Character_List = []
+    for i in range(len(Message_Character_List)):
+        if Message_Character_List[i]:
+            print(Character_Values.char_input_output_encrypting(Message_Character_List[i], key, Character_List), i, Message_Character_List[i])
+            Encrypted_Character_List.append(Character_Values.char_input_output_encrypting(Message_Character_List[i], key, Character_List))
+            key += 1
+        if key == 62:
+            key = 0
+    return Encrypted_Character_List
+
+
+def decryption(Encrypted_Character_List, key, Character_List):
+    if key > 0:
+        key = -key
+    if key < -61:
+        key = key % 61
+    Decrypted_Character_List = []
+    for i in range(len(Encrypted_Character_List)):
+        if Encrypted_Character_List[i]:
+            Decrypted_Character_List.append(Character_Values.char_input_output_encrypting(Encrypted_Character_List[i], key, Character_List))
+            key -= 1
+        if key == -62:
+            key = 0
+    return Decrypted_Character_List
+        
 
 
 if __name__ == "__main__":
-    message = character_list_generator()
-    decryption(message, 0)
+    message = message_list_generator()
+    Character_List = Character_Values.get_chars()
+    print(message)
+    Encrypted_Character_List = encryption(message, 3, Character_List)
+    #for i in range(len(Encrypted_Character_List)):
+    print(Encrypted_Character_List)
+    Decrypted_Character_List = decryption(Encrypted_Character_List, 3, Character_List)
+    print(Decrypted_Character_List)
